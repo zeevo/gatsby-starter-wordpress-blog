7d6f4fc3a578cc726c879f69e5054a35f9ced6f2
zeevosec <oneill.shane.h@gmail.com>
2020-03-20 17:03:37 -0400

    feat: change wordpress location

--- gatsby-config.js
+++ gatsby-config.js
@@ -197,7 +197,6 @@ module.exports = {
     {
       resolve: 'gatsby-source-wordpress',
       options: {
-        // baseUrl: 'http://ec2-3-15-175-195.us-east-2.compute.amazonaws.com/blog/index.php/',
         baseUrl: 'https://admin.zeevo.me/',
         protocol: 'http',
         hostingWPCOM: false,
